b87b0963752b1ae25fd6f0364fc8003826717623
Matrix <1337816495@qq.com>
2022-02-24 23:15:37 +0800

    u

--- generator.js
+++ generator.js
@@ -30,7 +30,7 @@ function generate( input_file, output_file ) {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Document</title>
-                <link rel="stylesheet" href="../style/resize.css">
+                <link rel="stylesheet" href="/style/resize.css">
             </head>
             <body>
                 <article>
