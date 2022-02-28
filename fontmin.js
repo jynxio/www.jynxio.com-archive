d2619a3f9a0473525a54564afd637c638cc0b8e0
Matrix <1337816495@qq.com>
2022-02-28 16:58:16 +0800

    u

---
+++ fontmin.js
@@ -0,0 +1,26 @@
+const Fontmin = require( "fontmin" );
+
+/* 待处理的字体文件的路径字符串 */
+const input = "./static/font/NotoSansSC-Thin.ttf";
+/* 文件夹路径，生成结果将会存储在该文件夹中 */
+const output = "./static/fontmin-test";
+/* 字符子集 */
+const character_set = "撒丢将坎坎坷坷奶茶妹过来第一次它那丝粗奥申委开车慢点开伤口";
+
+const fontmin = new Fontmin()
+    .src( input )
+    .use( Fontmin.glyph( { text: character_set } ) )
+    .use( Fontmin.ttf2woff2() )
+    .dest( output );
+
+fontmin.run( function( error, files, stream ) {
+
+    if ( error ) {
+
+        throw error;
+
+    }
+
+    console.log( "Done!" );
+
+} );
