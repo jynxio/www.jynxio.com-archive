dae4a8c0bd64ecb95f975da26317be0012ce538d
X <1337816495@qq.com>
2022-03-02 11:48:55 +0800

    u

--- generator.js
+++ generator.js
@@ -79,12 +79,14 @@ function translateMdToHtml( input, output ) {
         const font_en_bold_path = "./static/font/IBMPlexSerif-Medium.ttf";
         const font_core_path = "./static/font/FiraCode-Regular.ttf";
         const all_character_set = extractText( html );
+        const strong_character_set = extractText( html, [ "strong", "b" ] );
+        const code_character_set = extractText( html, [ "code" ] );
 
         optimizeFont( font_zh_thin_path, mini_font_path, all_character_set );
-        optimizeFont( font_zh_bold_path, mini_font_path, all_character_set );
         optimizeFont( font_en_thin_path, mini_font_path, all_character_set );
-        optimizeFont( font_en_bold_path, mini_font_path, all_character_set );
-        optimizeFont( font_core_path, mini_font_path, all_character_set );
+        optimizeFont( font_zh_bold_path, mini_font_path, strong_character_set );
+        optimizeFont( font_en_bold_path, mini_font_path, strong_character_set );
+        optimizeFont( font_core_path, mini_font_path, code_character_set );
 
     }
 
@@ -118,26 +120,67 @@ function optimizeFont( input, output, characters ) {
 }
 
 /**
- * 从html文件中提取出所有标签的内容，然后合并成一个字符串，最后返回这个字符串。
- * @param {string} input - html文件的内容，是一串字符串。
+ * 从html文件中提取出所有或指定标签的内容，然后合并成一段新的字符串，最后返回这段新的字符串。
+ * @param {string} input - html文件的内容，是一串字符串。注意，html文件的标签名称不区分大小写。
+ * @param {Array} [node_names] - 一个数组，它包含了零至多个标签的名称，比如["h1", "p"]。缺省时，返回结果将包含所有标签的内容，
+ *                               否则返回结果将只包含指定标签的内容。注意，1.不能输入自闭合标签；2.标签名称不区分大小写。
  * @returns {string} - 一个字符串，它包含了html文件中的所有标签的内容。
  */
-function extractText( input ) {
+ function extractText( input, nodes ) {
+
+    if ( !Array.isArray( nodes ) ) return core( input );
+
+    if ( !nodes.length ) return "";
+
+    const regexp = new RegExp( `</?(${ nodes.join( "|" ) })(>|(\\s*>)|(\\s[^>]*>))`, "ig" );
+    const tags = input.match( regexp );
+
+    if ( !tags ) return "";
 
     let output = "";
-    let starting_index = 0;
+    let from_index_in_tags = 0;
+    let from_index_in_input = 0;
 
-    input.match( /<!?\/?[a-z][a-z0-9]*[^>]*>/ig ).forEach( token => {
+    while ( from_index_in_tags < tags.length ) {
 
-        const endding_index = input.indexOf( token, starting_index );
-        const next_starting_index = endding_index + token.length;
-        const snippet = input.slice( starting_index, endding_index );
+        const header = tags[ from_index_in_tags ];
+        const footer = header.match( /<[a-z][a-z0-9]*/i )[ 0 ].replace( "<", "</" ) + ">";
 
-        output += snippet;
-        starting_index = next_starting_index;
+        const header_index = from_index_in_tags;
+        const footer_index = tags.indexOf( footer, from_index_in_tags );
 
-    } );
+        const from = input.indexOf( header, from_index_in_input );
+        const to = input.indexOf( footer, from_index_in_input ) + footer.length;
+
+        output += core( input.slice( from ,to ) );
+        from_index_in_tags = footer_index + 1;
+        from_index_in_input = to;
+
+    }
 
     return output;
+
+    function core( input ) {
+
+        const tags = input.match( /<!?\/?[a-z][a-z0-9]*[^>]*>/ig );
+
+        if ( !tags ) return "";
+
+        let output = "";
+        let from_index = 0;
+
+        tags.forEach( tag => {
+
+            const from = input.indexOf( tag, from_index );
+            const snippet = input.slice( from_index, from );
+
+            output += snippet;
+            from_index = from + tag.length;
+
+        } );
+
+        return output;
+
+    }
 
 }
