c77be1fe5764c61dcbc8d032675552f402ff5d77
X <1337816495@qq.com>
2022-03-23 10:54:31 +0800

    u

--- publisher/fontcaster/index.js
+++ publisher/fontcaster/index.js
@@ -2,6 +2,7 @@ const fontcaster = require( "fontcaster" );
 
 async function castFont() {
 
+    /* Read all html files. */
     const read_response = await fontcaster.read( "./page" );
 
     if ( ! read_response.success ) {
@@ -12,17 +13,44 @@ async function castFont() {
 
     }
 
+    /* Extract characters. */
+    let characters = "";
+
     for ( const file of read_response.files ) {
 
-        const { name, path, content } = file;
+        const { name, type, content } = file;
 
         if ( name === ".DS_Store" ) continue;
 
-        const html_content = fontcaster.parseHtml( content );
+        if ( type !== "html" ) continue;
 
-        // console.log( html_content );
+        characters += fontcaster.deduplication( fontcaster.parseHtml( content ) );
 
     }
+
+    /* Subset font file. */
+    const origin_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular.woff";
+    const subset_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular-subset.woff";
+
+    const subset_response = await fontcaster.subset( characters, origin_font_path, subset_font_path );
+
+    if ( ! subset_response.success ) {
+
+        console.error( "SUBSET ERROR: " + subset_response.error );
+
+        return;
+
+    }
+
+    const subset_information = subset_response.information;
+
+    // console.log( subset_information.successfulCharacters  );
+    // console.log( subset_information.successfulUnicodes  );
+    // console.log( subset_information.failedCharacters  );
+    // console.log( subset_information.failedUnicodes  );
+
+    /* Output characters. */
+    // TODO
 
 }
 
