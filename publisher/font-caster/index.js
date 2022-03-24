efe31114c89b6df63a67dd20835ccbd6b336df4d
X <1337816495@qq.com>
2022-03-24 16:08:05 +0800

    u

--- publisher/font-caster/index.js
+++ publisher/font-caster/index.js
@@ -1,15 +1,30 @@
 const fontcaster = require( "font-caster" );
 
-async function castFont() {
+// BUG: read空文本时会产生bug。
+// BUG: convert 会有这个bug吗？
+
+/**
+ * 字体子集化。
+ * @param { string } html_files_path - 一个html文本的路径，或存储零至多个html文本的目录的路径，比如"./page/example.html"或"./page"。
+ * @param { string } txt_file_path - txt文本的路径，比如"./static/font/character-set.txt"。txt文本的内容是以逗号分隔的unicode（基
+ * 于十进制）。该方法将使用txt字符集与html字符集的并集（自动进行字符去重）来进行字体子集化，最后并集将以逗号分隔的unicode的形式覆写回txt文本中。
+ * 如果你是第一次进行字体子集化，请提供空的txt文本。如果你是第二次进行字体子集化，请提供上一次子集化所产生的txt文本，这样本次子集化所产生的字体文件
+ * 将包含两次子集化的字符集。
+ * @param { string } origin_font_path - 原始的字体文件的路径，比如 "./origin.otf"，也支持 ttf、woff。该方法不会改变原始的字体文件。
+ * @param { string } subset_font_path - 生成的字体文件的路径，比如 "./sunset.otf"，生成的字体文件的格式必须与生原始的字体文件的格式一致。
+ * 若存在同名文件，则该方法会覆盖同名文件。
+ * @returns { Promise } - Promise代表是否子集化成功，若成功则返回{success: true}对象，否则返回{success: false, error}对象。
+ */
+async function castFont( html_files_path, txt_file_path,  origin_font_path, subset_font_path ) {
 
     /* Read all html files. */
-    const read_response = await fontcaster.read( "./page" );
+    const read_response = await fontcaster.read( html_files_path );
 
     if ( ! read_response.success ) {
 
-        console.error( "READ ERROR: " + read_response.error );
+        console.error( "Read Error: ", read_response.error );
 
-        return;
+        return { success: false, error: read_response.error };
 
     }
 
@@ -31,51 +46,80 @@ async function castFont() {
     characters = fontcaster.deduplication( characters );
 
     /* Subset font file. */
-    const origin_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular.woff";
-    const subset_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular-subset.woff";
-
     const subset_response = await fontcaster.subset( characters, origin_font_path, subset_font_path );
 
     if ( ! subset_response.success ) {
 
-        console.error( "SUBSET ERROR: " + subset_response.error );
+        console.error( "Subset error: ", subset_response.error );
 
-        return;
-
-    }
-
-    printSubsetInformation( subset_response.information );
-
-    /* Output characters. */
-    const write_response = await fontcaster.write( fontcaster.convert( characters ), "./static/font/character-set.txt" );
-
-    if ( ! write_response.success ) {
-
-        console.error( "WRITE ERROR: " + write_response.error );
-
-        return;
+        return { success: false, error: subset_response.error };
 
     }
 
-}
+    /* Print information about subset. */
+    const print_subset_information_options = {
 
-function printSubsetInformation( info ) {
+        printTotalNumber: true,
 
-    const success_unicodes = info.successfulUnicodes;
-    const success_characters = info.successfulCharacters;
+        printSuccessfulNumber: true,
 
-    const failed_unicodes = info.failedUnicodes;
-    const failed_characters = info.failedCharacters;
+        printFailedNumber: true,
 
-    const number_success = success_unicodes.length;
-    const number_failed = failed_unicodes.length;
-    const number_total = number_success + number_failed;
+        printSuccessfulUnicodes: false,
 
-    console.warn( "------------------------ SUBSET INFORMATION ------------------------" );
-    console.warn( "Total            : " + number_total );
-    console.warn( "Number of success: " + number_success );
-    console.warn( "Number of failed : " + number_failed );
-    console.warn( "------------------------ SUBSET INFORMATION ------------------------" );
+        printSuccessfulCharacters: false,
+
+        printFailedUnicodes: false,
+
+        printFailedCharacters: false,
+
+    };
+
+    printInformationAboutSubset( subset_response.information, print_subset_information_options );
+
+    /* The end. */
+    return { success: true };
+
+    function printInformationAboutSubset( information, {
+
+        printTotalNumber = true,
+
+        printSuccessfulNumber = true,
+
+        printFailedNumber = true,
+
+        printSuccessfulUnicodes = false,
+
+        printSuccessfulCharacters = false,
+
+        printFailedUnicodes = false,
+
+        printFailedCharacters = false,
+
+    } = {} ) {
+
+        const successful_unicodes = information.successfulUnicodes;
+        const successful_characters = information.successfulCharacters
+
+        const failed_unicodes = information.failedUnicodes;
+        const failed_characters = information.failedCharacters;
+
+        const successful_number = successful_unicodes.length;
+        const failed_number = failed_unicodes.length;
+
+        const total_number = successful_number + failed_number;
+
+        console.log( "--------------------------- Subset Information ---------------------------"  );
+        printTotalNumber          && console.log( "Total number:          ", total_number          );
+        printSuccessfulNumber     && console.log( "Successful number:     ", successful_number     );
+        printFailedNumber         && console.log( "Failed number:         ", failed_number         );
+        printSuccessfulUnicodes   && console.log( "Successful unicodes:   ", successful_unicodes   );
+        printSuccessfulCharacters && console.log( "Successful characters: ", successful_characters );
+        printFailedUnicodes       && console.log( "Failed unicodes:       ", failed_unicodes       );
+        printFailedCharacters     && console.log( "Failed characters:     ", failed_characters     );
+        console.log( "--------------------------- Subset Information ---------------------------"  );
+
+    }
 
 }
 
--- publisher/index.js
+++ publisher/index.js
@@ -1,3 +1,32 @@
+const fontcaster = require( "font-caster" );
 const castFont = require( "./font-caster/index" );
 
-castFont();
+const html_files_path = "./page";
+const origin_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular.woff";
+const subset_font_path = "./static/font/noto-sans-sc-v24-chinese-simplified-regular-subset.woff"
+
+// castFont( html_files_path, origin_font_path, subset_font_path ).then( response => {
+
+//     if ( ! response.success ) return;
+
+//     console.log( "SUCCESS!" );
+
+// } );
+
+fontcaster.read( "./static/font/character-set.txt", true ).then( response => {
+
+    if ( ! response.success ) {
+
+        console.error( "Error: ", response.error );
+
+        return;
+
+    }
+
+    for ( const file of response.files ) {
+
+        console.log( file.name, file.content );
+
+    }
+
+} );
