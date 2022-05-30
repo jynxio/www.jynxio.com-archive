0b955d39c63f43f85c0a4ae21112a600484b2d27
Matrix <1337816495@qq.com>
2022-05-30 14:45:09 +0800

    Optimize the article page
    1.Optimize the navigation bar style
    2.Optimize the implemented logic of the navigation bar
    3.Optimize page positioning: the page jumps to the top instead of some other location after refreshing

--- source/article/index.js
+++ source/article/index.js
@@ -2,42 +2,36 @@ import "/style/public/reset.css";
 import "/style/public/font.css";
 import "/style/article/index.css";
 
-const aside = document.querySelector( "aside" );
-let aside_width;
-let aside_height;
-
-window.addEventListener( "load", _ => {
-
-    const { width, height } = aside.getBoundingClientRect();
-
-    aside_width = width;
-    aside_height = height;
-
-    canYouDisplayAside();
-    aside.style.visibility = "visible";
-    window.addEventListener( "resize", _ => canYouDisplayAside() );
-
-} );
-
 /**
  * 若页面的尺寸足够容纳aside，则显示aside，否则隐藏aside。
  */
-function canYouDisplayAside() {
+globalThis.addEventListener( "load", _ => {
 
-    /* 页面的尺寸 */
-    const viewport_width = window.innerWidth;
-    const viewport_height = window.innerHeight;
+    const aside = document.querySelector( "aside" );
+    const { width: aside_width, height: aside_height } = aside.getBoundingClientRect();
 
-    /* 计算页面的尺寸是否足以容纳aside */
-    const article_width = Math.min( viewport_width, viewport_height ); // 100vmin，因为page.css规定了article的width是100vmin。
+    displayOrHiddenAside();
+    aside.style.visibility = "visible";
+    globalThis.addEventListener( "resize", _ => displayOrHiddenAside() );
 
-    const is_width_enough = ( viewport_width - article_width ) / 2 >= aside_width;
-    const is_height_enough = viewport_height >= aside_height;
+    function displayOrHiddenAside() {
 
-    /* 显示/隐藏 */
-    aside.style.display = is_width_enough && is_height_enough ? "block" : "none";
+        /* 页面的尺寸 */
+        const viewport_width = globalThis.innerWidth;
+        const viewport_height = globalThis.innerHeight;
 
-}
+        /* 计算页面的尺寸是否足以容纳aside */
+        const article_width = Math.min( viewport_width, viewport_height ); // 100vmin，因为page.css规定了article的width是100vmin。
+
+        const is_width_enough = ( viewport_width - article_width ) / 2 >= aside_width;
+        const is_height_enough = viewport_height >= aside_height;
+
+        /* 显示/隐藏 */
+        aside.hidden = ( is_width_enough && is_height_enough ) ? false : true;
+
+    }
+
+} );
 
 /**
  * 禁止页面刷新后自动滚动至历史位置
