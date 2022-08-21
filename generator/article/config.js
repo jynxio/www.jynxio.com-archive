b49ee65e53aef4d4effd7f875f98670c90c03eb0
Matrix <1337816495@qq.com>
2022-08-21 14:47:16 +0800

    Update config.js

--- generator/article/config.js
+++ generator/article/config.js
@@ -75,6 +75,7 @@ const createDataStructureInfo = _ => {
         { name: "dictionary", alias: "字典" },
         { name: "hash-table", alias: "散列表" },
         { name: "recursion", alias: "递归" },
+        { name: "tree", alias: "树" },
     ];
     const options = {
         name,
