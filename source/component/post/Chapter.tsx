6f9cb188e70c4b4ad03f9541e8c4bcf145b1dae0
jynxio <1337816495@qq.com>
2023-04-14 18:21:45 +0800

    🌱 push

--- source/component/post/Chapter.tsx
+++ source/component/post/Chapter.tsx
@@ -13,11 +13,7 @@ function Chapter () {
 					<>
 						<div class={ style.h1 } classList={ { [ style.selected ]: isTargetChapter( h1Node.uuid ) } } onClick={ [ handleClick, h1Node.uuid ] }>
 							<span class={ style.icon }>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-1">
-									<path d="M4 12h8" /><path d="M4 18V6" />
-									<path d="M12 18V6" />
-									<path d="m17 12 3-2v8" />
-								</svg>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-2"><path d="M4 12h8" /><path d="M4 18V6" /><path d="M12 18V6" /><path d="M21 18h-4c0-4 4-3 4-6 0-1.5-2-2.5-4-1" /></svg>
 							</span>
 							<span class={ style.name }>{ h1Node.name }</span>
 						</div>
@@ -25,12 +21,7 @@ function Chapter () {
 							h2Node => (
 								<div class={ style.h2 } classList={ { [ style.selected ]: isTargetChapter( h2Node.uuid ) } } onClick={ [ handleClick, h2Node.uuid ] }>
 									<span class={ style.icon }>
-										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-2">
-											<path d="M4 12h8" />
-											<path d="M4 18V6" />
-											<path d="M12 18V6" />
-											<path d="M21 18h-4c0-4 4-3 4-6 0-1.5-2-2.5-4-1" />
-										</svg>
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-3"><path d="M4 12h8" /><path d="M4 18V6" /><path d="M12 18V6" /><path d="M17.5 10.5c1.7-1 3.5 0 3.5 1.5a2 2 0 0 1-2 2" /><path d="M17 17.5c2 1.5 4 .3 4-1.5a2 2 0 0 0-2-2" /></svg>
 									</span>
 									<span class={ style.name }>{ h2Node.name }</span>
 								</div>
--- source/component/post/Content.tsx
+++ source/component/post/Content.tsx
@@ -1,23 +1,37 @@
+import style from "./Content.module.css";
 import * as postCatalogStore from "@/store/postCatalog";
 import * as chapterCatalogStore from "@/store/chapterCatalog";
-import { createEffect, createUniqueId } from "solid-js";
+import { createEffect, createSignal, createUniqueId } from "solid-js";
 import { marked } from "marked";
 
+const LINK_SVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-external-link\"><path d=\"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6\"></path><polyline points=\"15 3 21 3 21 9\"></polyline><line x1=\"10\" x2=\"21\" y1=\"14\" y2=\"3\"></line></svg>";
+
 function Content () {
+
+	const [ getHtml, setHtml ] = createSignal( "" );
 
 	createEffect( () => {
 
 		const url = postCatalogStore.getPostUrl();
 
-		if ( url === void 0 ) return;
+		if ( url === void 0 ) {
+
+			setHtml( "" );
+			chapterCatalogStore.setData( void 0 );
+
+			return;
+
+		}
 
 		fetch( url )
 			.then( res => res.text() )
-			.then( txt => parseMarkdown( txt ) );
+			.then( txt => setHtml( parseMarkdown( txt ) ) );
 
 	} );
 
-	return <div />;
+	return (
+		<article class={ style.content } innerHTML={ getHtml() } />
+	);
 
 }
 
@@ -26,48 +40,74 @@ function parseMarkdown ( markdown: string ) {
 	type H2Node = { name: string, uuid: string };
 	type H1Node = { name: string, uuid: string, children: H2Node[] };
 
-	const chapterCatalogData = [] as H1Node[];
-
 	marked.use( {
 		gfm: true,
 		xhtml: true,
 		async: false,
 		renderer: {
 			heading: parseHeading,
+			hr: parseHr,
+			link: parseLink,
 		},
 	} );
 
-	const html = marked.parse( markdown );
+	const chapterCatalogData = [] as H1Node[];
 
+	let title: string | undefined;
+	let html = marked.parse( markdown );
+
+	if ( title === void 0 ) throw new Error( "Markdown format: every markdown document must have an h1 tag" );
+
+	html = title + html;
 	chapterCatalogStore.setData( chapterCatalogData );
 
 	return html;
 
 	function parseHeading ( text: string, level: number ) {
 
-		if ( level === 1 ) return "";
-
-		if ( level > 3 ) throw new Error( "Markdown Error: can not process h4, h5, h6" );
-
 		const uuid = createUniqueId();
 
-		if ( level === 2 ) {
+		switch ( level ) {
 
-			const h1Node = { name: text, uuid, children: [] };
+		case 1:
+			if ( title !== void 0 ) throw new Error( "Markdown format: each markdown document only allows one h1 tag to exist" );
 
-			chapterCatalogData.push( h1Node );
+			title = `<h1 id="${ uuid }">${ text }</h1>`;
 
-		}
+			return "";
 
-		if ( level === 3 ) {
+		case 2:
+			if ( text === "typora-root-url: ...." ) return "";
 
-			const h2Node = { name: text, uuid };
+			chapterCatalogData.push( { name: text, uuid, children: [] } );
 
-			chapterCatalogData.at( - 1 )!.children.push( h2Node );
+			break;
+
+		case 3:
+			chapterCatalogData.at( - 1 )!.children.push( { name: text, uuid } );
+
+			break;
+
+		default:
+			throw new Error( "Markdown format: h4, h5, and h6 tags are not allowed" );
 
 		}
 
 		return `<h${ level } id="${ uuid }">${ text }</h${ level }>`;
+
+	}
+
+	function parseHr () {
+
+		return "";
+
+	}
+
+	function parseLink ( href: string, title: string, text: string ) {
+
+		console.log( href, title, text );
+
+		return `<a href="${ href }" target="_blank">${ text + LINK_SVG }</a>`;
 
 	}
 
