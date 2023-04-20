30ff3a3bc6c4ba023347890bc3d7c7fcce7b4f19
jynxio <1337816495@qq.com>
2023-04-20 23:52:50 +0800

    🌱 push

---
+++ source/subset/read.js
@@ -0,0 +1,14 @@
+const fs = require( "fs" );
+const path = require( "path" );
+
+/**
+ * 读取指定文件或指定目录下的所有文件
+ * @param { string } path 文件或文件夹的路径
+ */
+async function read ( path ) {
+
+	/* TODO */
+
+}
+
+module.exports = { read };
