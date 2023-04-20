9cbbec234bbcdf606d7fb72b92af01802f663de4
jynxio <1337816495@qq.com>
2023-04-20 18:15:37 +0800

    🌱 push

--- source/page/Post.tsx
+++ source/page/Post.tsx
@@ -2,15 +2,8 @@ import style from "./Post.module.css";
 import Nav from "@/component/post/Nav";
 import Chapter from "@/component/post/Chapter";
 import Content from "@/component/post/Content";
-import Scrollbar from "@/component/common/Scrollbar";
-import { onCleanup, onMount } from "solid-js";
 
 function Post () {
-
-	onMount( () => globalThis.addEventListener( "scroll", handleScroll ) );
-	onCleanup( () => globalThis.removeEventListener( "scroll", handleScroll ) );
-
-	// TODO
 
 	return (
 		<div class={ style.post }>
@@ -23,17 +16,8 @@ function Post () {
 			<section class={ style.chapter }>
 				<Chapter />
 			</section>
-			<section class={ style.scrollbar }>
-				<Scrollbar display={ true } size={ 0.1 } position={ 0.5 } />
-			</section>
 		</div>
 	);
-
-	function handleScroll () {
-
-		console.log( document.documentElement.offsetHeight, document.documentElement.scrollTop );
-
-	}
 
 }
 
