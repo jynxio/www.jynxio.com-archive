75358afd8ed8d100e9f5138fe2b1d0bd17295e38
jynxio <1337816495@qq.com>
2023-05-01 18:59:16 +0800

    🐛 fix shortcut

--- source/component/post/Nav.tsx
+++ source/component/post/Nav.tsx
@@ -1,6 +1,7 @@
 import style from "./Nav.module.css";
 import Theme from "@/component/common/Theme";
 import catalogData from "@/asset/catalog/data.json";
+import checkOs from "@/helper/checkOs";
 import * as searchStore from "@/store/search";
 import { useNavigate, useParams } from "@solidjs/router";
 import { For, Show, createSelector, createSignal } from "solid-js";
@@ -27,7 +28,7 @@ function Search () {
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-terminal"><polyline points="4 17 10 11 4 5" /><line x1="12" x2="20" y1="19" y2="19" /></svg>
 			</span>
 			<span class={ style.text }>搜索</span>
-			<span class={ style.shortcut }>⌘ + K</span>
+			<kbd class={ style.shortcut }>{ checkOs() === "macOS" ? "⌘" : "Ctrl" } K</kbd>
 		</section>
 	);
 
