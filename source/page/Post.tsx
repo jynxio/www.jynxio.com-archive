0cca9694551bff1a6170f9589e4189bc7c36134b
jynxio <1337816495@qq.com>
2023-05-19 17:40:27 +0800

    🌱 Optimize lazy loading

--- source/page/Post.tsx
+++ source/page/Post.tsx
@@ -1,26 +1,20 @@
 import style from "./Post.module.css";
 import Nav from "@/component/post/Nav";
-import { Show, lazy } from "solid-js";
-import { useParams } from "@solidjs/router";
-import routerHelper from "@/helper/routerHelper";
+import { lazy } from "solid-js";
 
 const LazyContent = lazy( () => import( "@/component/post/Content" ) );
 const LazySearch = lazy( () => import( "@/component/post/Search" ) );
 
 function Post () {
 
-	const params = useParams();
-
 	return (
 		<div class={ style.post }>
 			<section class={ style.nav }>
 				<Nav />
 			</section>
-			<Show when={ routerHelper.post.checkPath( params.path ) }>
-				<section class={ style.content }>
-					<LazyContent />
-				</section>
-			</Show>
+			<section class={ style.content }>
+				<LazyContent />
+			</section>
 			<LazySearch />
 		</div>
 	);
