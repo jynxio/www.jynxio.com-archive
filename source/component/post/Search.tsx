0cd0fd0351d9f69d4c808122bb4f327c4c464d24
jynxio <1337816495@qq.com>
2023-06-15 22:57:03 +0800

    🌱 Adjust the style of the search bar

--- source/component/post/Search.tsx
+++ source/component/post/Search.tsx
@@ -52,7 +52,7 @@ function Search () {
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8" /><line x1="21" x2="16.65" y1="21" y2="16.65" /></svg>
 						</span>
 						<span>
-							<input type="text" placeholder="搜索" onInput={ handleInput } ref={ inputRef }/>
+							<input type="text" placeholder=" 搜索" onInput={ handleInput } ref={ inputRef }/>
 						</span>
 					</section>
 					<Show when={ getList().length }><hr /></Show>
