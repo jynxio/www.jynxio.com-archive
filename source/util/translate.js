664fb9fbdcaf8df37ec8d201c2dbebce348e3bc7
jynxio <1337816495@qq.com>
2023-07-03 00:43:53 +0800

    🌱 Add shiki.js

--- source/util/translate.js
+++ source/util/translate.js
@@ -1,5 +1,6 @@
-import { readFile, writeFile } from "node:fs/promises";
 import util from "node:util";
+import shiki from "shiki";
+import { readFile, writeFile } from "node:fs/promises";
 import { nanoid } from "nanoid";
 import { fromMarkdown } from "mdast-util-from-markdown";
 import { gfm } from "micromark-extension-gfm";
@@ -7,6 +8,8 @@ import { gfmFromMarkdown } from "mdast-util-gfm";
 import { toHast } from "mdast-util-to-hast";
 import { toHtml } from "hast-util-to-html";
 import { parseSync } from "svgson";
+
+const highlighter = await shiki.getHighlighter( { theme: "nord", langs: [ "javascript" ] } );
 
 const SVG_STRING_LINK = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-external-link\"><path d=\"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6\"></path><polyline points=\"15 3 21 3 21 9\"></polyline><line x1=\"10\" x2=\"21\" y1=\"14\" y2=\"3\"></line></svg>";
 const SVG_STRING_CHECKBOX = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-check-square\"><polyline points=\"9 11 12 14 22 4\"></polyline><path d=\"M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11\"></path></svg>";
@@ -171,6 +174,11 @@ function processCodeNode ( node ) {
 	node.type = "root";
 	node.data = {};
 	node.data.hChildren = [ pre2mast( node.value ) ];
+
+	const tokens = highlighter.codeToThemedTokens( node.value, "javascript" );
+	const html = shiki.renderToHtml( tokens );
+
+	// Const html = highlighter.codeToHtml( node.value, { lang: "js" } );
 
 }
 
