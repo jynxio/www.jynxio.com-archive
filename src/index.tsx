c6dbb3a0cb09df1d2607d7fc30e8e0201a5884c4
jynxio <1337816495@qq.com>
2023-07-25 02:18:50 +0800

    🌱 Update font

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import '@/reset.css';
 import '@/global.css';
+import '@/temps/fonts/lxgwwenkai-regular-400/lxgwwenkai-regular-400.css';
+import '@/temps/fonts/lxgwwenkai-bold-700/lxgwwenkai-bold-700.css';
+import '@/temps/fonts/lxgwwenkaimono-regular-400/lxgwwenkaimono-regular-400.css';
+import '@/temps/fonts/firacode-regular-400/firacode-regular-400.css';
 import Blog from '@/pages/Blog';
 import { render } from 'solid-js/web';
 import { Route, Router, Routes } from '@solidjs/router';
