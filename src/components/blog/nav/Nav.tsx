55332e77bd861879cfdf939bc2a02f5da6717a9d
jynxio <jinxiaomatrix@gmail.com>
2023-10-25 02:58:01 +0800

    🌱 Enhance Accessibility

--- src/components/blog/nav/Nav.tsx
+++ src/components/blog/nav/Nav.tsx
@@ -149,13 +149,12 @@ function Control() {
                         stroke-width="2"
                         stroke-linecap="round"
                         stroke-linejoin="round"
-                        class="lucide lucide-door-open"
+                        class="lucide lucide-tent"
                     >
-                        <path d="M13 4h3a2 2 0 0 1 2 2v14" />
-                        <path d="M2 20h3" />
-                        <path d="M13 20h9" />
-                        <path d="M10 12v.01" />
-                        <path d="M13 4.562v16.157a1 1 0 0 1-1.242.97L5 20V5.562a2 2 0 0 1 1.515-1.94l4-1A2 2 0 0 1 13 4.561Z" />
+                        <path d="M3.5 21 14 3" />
+                        <path d="M20.5 21 10 3" />
+                        <path d="M15.5 21 12 15l-3.5 6" />
+                        <path d="M2 21h20" />
                     </svg>
                     <span class={style.identifier}>homepage</span>
                 </a>
--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -8,11 +8,11 @@ const Search = lazy(() => import('@/components/blog/search'));
 function Blog() {
     return (
         <main class={style.container}>
-            <section class={style.nav}>
-                <Nav />
-            </section>
             <section class={style.post}>
                 <Post />
+            </section>
+            <section class={style.nav}>
+                <Nav />
             </section>
             <Search />
         </main>
