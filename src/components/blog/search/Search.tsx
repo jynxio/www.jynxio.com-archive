44924600944fdb2ee03e878d51576320f5af38b3
jynxio <jinxiaomatrix@gmail.com>
2023-12-12 21:37:52 +0800

    Fix: Imperfect behavior of the search bar

--- src/components/blog/search/Search.tsx
+++ src/components/blog/search/Search.tsx
@@ -1,11 +1,10 @@
 import style from './Search.module.css';
-import checkOs from '@/utils/checkOs';
 import data from '@/temps/configs/detailCatalog.json';
 import Fuse from 'fuse.js';
 import * as store from '@/stores/search';
 import { Portal } from 'solid-js/web';
 import { A } from '@solidjs/router';
-import { For, Show, batch, createSelector, createSignal, onCleanup, onMount } from 'solid-js';
+import { For, Show, batch, createSignal, onCleanup, onMount } from 'solid-js';
 
 /**
  * Type declaration
@@ -17,205 +16,225 @@ type List = Item[];
  * Fuzzy search
  */
 const fuse = new Fuse(data, {
-    includeScore: true,
-    includeMatches: true,
-    threshold: 0.2,
-    minMatchCharLength: 1,
-    keys: [['children', 'alias']],
+	includeScore: true,
+	includeMatches: true,
+	threshold: 0.2,
+	minMatchCharLength: 1,
+	keys: [['children', 'alias']],
 });
 
 function Search() {
-    let barRef: HTMLDivElement | undefined;
-    let sectionRef: HTMLElement | undefined;
+	let barRef: HTMLDivElement | undefined;
+	let optionRef: HTMLElement | undefined;
 
-    const [getSelectedIndex, setSelectedIndex] = createSignal(-1); // -1 represents that no one has been selected
-    const [getList, setList] = createSignal<List>([]);
-    const isSelected = createSelector(getSelectedIndex);
+	const [getList, setList] = createSignal<List>([]);
+	const [getSelectedIndex, setSelectedIndex] = createSignal(-1); // -1 represents that no one has been selected
 
-    onMount(() => {
-        document.addEventListener('pointerdown', handleClose);
-        document.addEventListener('keydown', handleShortcut);
-        document.addEventListener('keydown', handleSelect);
-    });
-    onCleanup(() => {
-        document.removeEventListener('pointerdown', handleClose);
-        document.removeEventListener('keydown', handleShortcut);
-        document.removeEventListener('keydown', handleSelect);
-    });
+	onMount(() => {
+		document.addEventListener('pointerdown', handleEscape);
+		document.addEventListener('keydown', handleShortcut);
+		document.addEventListener('keydown', handleSelect);
+	});
+	onCleanup(() => {
+		document.removeEventListener('pointerdown', handleEscape);
+		document.removeEventListener('keydown', handleShortcut);
+		document.removeEventListener('keydown', handleSelect);
+	});
 
-    return (
-        <Portal mount={document.querySelector('body')!}>
-            <Show when={store.getEnabled()}>
-                <aside class={style.container} style={{ display: store.getEnabled() ? '' : 'none' }}>
-                    <div class={style.bar} ref={barRef}>
-                        <section class={style.input}>
-                            <span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="lucide lucide-search"
-                                >
-                                    <circle cx="11" cy="11" r="8" />
-                                    <line x1="21" x2="16.65" y1="21" y2="16.65" />
-                                </svg>
-                            </span>
-                            <span>
-                                <input
-                                    type="text"
-                                    placeholder=" 搜索"
-                                    onInput={handleInput}
-                                    ref={ref => Promise.resolve().then(() => (ref.focus(), (ref.value = '')))}
-                                />
-                            </span>
-                        </section>
-                        <Show when={getList().length}>
-                            <hr />
-                        </Show>
-                        <section class={style.output} ref={sectionRef}>
-                            <For each={getList()}>
-                                {(item, getIndex) => (
-                                    <A
-                                        href={`/blog/${item.topicName}/${item.postName}`}
-                                        class={style.link}
-                                        innerHTML={item.html}
-                                        classList={{ [style.selected]: isSelected(getIndex()) }}
-                                        onPointerEnter={[handleHover, getIndex]}
-                                        onPointerLeave={[handleBlur, getIndex]}
-                                        onPointerDown={[handleNavigate, getIndex]}
-                                    />
-                                )}
-                            </For>
-                        </section>
-                    </div>
-                </aside>
-            </Show>
-        </Portal>
-    );
+	return (
+		<Portal mount={document.querySelector('body')!}>
+			<Show when={store.getEnabled()}>
+				<aside class={style.container} style={{ display: store.getEnabled() ? '' : 'none' }}>
+					<div class={style.bar} ref={barRef}>
+						<section class={style.input}>
+							<span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="lucide lucide-search"
+								>
+									<circle cx="11" cy="11" r="8" />
+									<line x1="21" x2="16.65" y1="21" y2="16.65" />
+								</svg>
+							</span>
+							<span>
+								<input
+									type="text"
+									placeholder=" 搜索"
+									onInput={handleInput}
+									ref={ref => Promise.resolve().then(() => (ref.focus(), (ref.value = '')))}
+								/>
+							</span>
+						</section>
+						<Show when={getList().length}>
+							<hr />
+						</Show>
+						<section class={style.output} ref={optionRef} onPointerDown={handleClick}>
+							<For each={getList()}>
+								{(item, getIndex) => (
+									<A
+										href={`/blog/${item.topicName}/${item.postName}`}
+										class={style.link}
+										innerHTML={item.html}
+										onPointerLeave={[handleBlur, getIndex]}
+										onPointerEnter={[handleFocus, getIndex]}
+									/>
+								)}
+							</For>
+						</section>
+					</div>
+				</aside>
+			</Show>
+		</Portal>
+	);
 
-    function handleInput(event: InputEvent) {
-        const target = event.target as HTMLInputElement;
-        const data = fuse.search(target.value);
-        const newList: List = createList(data);
+	function handleInput(event: InputEvent) {
+		const target = event.target as HTMLInputElement;
+		const data = fuse.search(target.value);
+		const newList: List = createList(data);
 
-        setList(newList);
-    }
+		setList(newList);
+	}
 
-    function handleClose(event: PointerEvent) {
-        if (!barRef) return;
+	function handleEscape(event: PointerEvent) {
+		const target = event.target as HTMLElement;
+		const isFocus = barRef?.contains(target);
 
-        const target = event.target as HTMLElement;
-        const isFocus = barRef.contains(target);
+		isFocus || close();
+	}
 
-        isFocus || store.setEnabled(false);
-    }
+	function handleBlur(_: () => number, event: PointerEvent) {
+		const target = event.target as HTMLLinkElement;
 
-    function handleNavigate(getIndex: () => number) {
-        const link = sectionRef?.children[getIndex()] as HTMLLinkElement | undefined;
+		target.blur();
+		setSelectedIndex(-1);
+	}
 
-        link?.click();
-        store.setEnabled(false);
-    }
+	function handleFocus(getIndex: () => number, event: PointerEvent) {
+		const target = event.target as HTMLLinkElement;
 
-    function handleBlur() {
-        setSelectedIndex(-1);
-    }
+		target.focus();
+		setSelectedIndex(getIndex());
+	}
 
-    function handleHover(getIndex: () => number) {
-        setSelectedIndex(getIndex());
-    }
+	function handleClick(event: PointerEvent) {
+		// Note: 此函数会立即注销掉目标元素，而默认事件（指跳转）发生在此函数之后，因此必须手动调用click。
+		//       如果可以，passive是更加可读的选择，可惜JSX中总是无法使用该特性。
+		const target = event.target as HTMLElement;
 
-    function handleSelect(event: KeyboardEvent) {
-        if (getList().length === 0) return;
+		if (target.tagName.toLowerCase() !== 'a') return;
 
-        const key = event.key.toLowerCase();
+		target.click();
+		target.blur();
+		event.preventDefault();
+		close();
+	}
 
-        if (key !== 'arrowup' && key !== 'arrowdown' && key !== 'enter') return;
+	function handleSelect(event: KeyboardEvent) {
+		if (!store.getEnabled()) return;
+		if (getList().length === 0) return;
 
-        /* Key: arrow up */
-        if (key === 'arrowup')
-            return void (getSelectedIndex() <= 0
-                ? setSelectedIndex(getList().length - 1)
-                : setSelectedIndex(prev => prev - 1));
+		const key = event.key.toLowerCase();
 
-        /* Key: arrow down */
-        if (key === 'arrowdown') return void setSelectedIndex(prev => (prev + 1) % getList().length);
+		/* Key: arrow up */
+		if (key === 'arrowup') {
+			const prevIndex = getSelectedIndex();
+			const nextIndex = prevIndex <= 0 ? getList().length - 1 : prevIndex - 1;
 
-        /* Key: enter */
-        if (getSelectedIndex() === -1) return;
+			setSelectedIndex(nextIndex);
+			(optionRef?.children[nextIndex] as HTMLLinkElement)?.focus();
+			return;
+		}
 
-        const link = sectionRef?.children[getSelectedIndex()] as HTMLLinkElement;
+		/* Key: arrow down */
+		if (key === 'arrowdown' || key === 'tab') {
+			const prevIndex = getSelectedIndex();
+			const nextIndex = (prevIndex + 1) % getList().length;
 
-        link.click();
-        store.setEnabled(false);
-    }
+			event.preventDefault();
+			setSelectedIndex(nextIndex);
+			(optionRef?.children[nextIndex] as HTMLLinkElement)?.focus();
+			return;
+		}
 
-    function handleShortcut(event: KeyboardEvent) {
-        const key = event.key.toLowerCase();
-        const isPreKeyDown = checkOs() === 'macOS' ? event.metaKey : event.ctrlKey;
-        const enabled =
-            key === 'k' && isPreKeyDown
-                ? !store.getEnabled() // Ctrl+k or ⌘+k => switch
-                : key === 'escape' && store.getEnabled()
-                ? false // Escape => close
-                : key === '/' && !store.getEnabled()
-                ? true // Slash => open
-                : store.getEnabled(); // Nothing
+		/* Key: enter */
+		if (key === 'enter') {
+			if (getSelectedIndex() === -1) return;
 
-        if (enabled === store.getEnabled()) return;
+			(optionRef?.children[getSelectedIndex()] as HTMLLinkElement)?.click();
+			close();
+		}
+	}
 
-        event.preventDefault();
-        batch(() => {
-            enabled && setList([]); // 清除历史搜索记录
-            store.setEnabled(enabled);
-        });
-    }
+	function handleShortcut(event: KeyboardEvent) {
+		const key = event.key.toLowerCase();
+		const enabled =
+			key === '/' && !store.getEnabled()
+				? true
+				: key === 'escape' && store.getEnabled()
+					? false
+					: store.getEnabled();
+
+		if (enabled === store.getEnabled()) return;
+
+		event.preventDefault();
+		enabled ? store.setEnabled(true) : close();
+	}
+
+	function close() {
+		batch(() => {
+			setList([]);
+			setSelectedIndex(-1);
+			store.setEnabled(false);
+		});
+	}
 }
 
 function createList(data: any) {
-    const list: List = [];
+	const list: List = [];
 
-    for (const { item: topic, matches } of data) {
-        if (matches === void 0) continue;
+	for (const { item: topic, matches } of data) {
+		if (matches === void 0) continue;
 
-        for (const { refIndex, indices } of matches) {
-            if (refIndex === void 0) continue;
+		for (const { refIndex, indices } of matches) {
+			if (refIndex === void 0) continue;
 
-            const post = topic.children[refIndex];
-            const categoryHtml = `<span>${topic.alias}</span>`;
-            const postHtmlPrefix = '<span>';
-            const postHtmlSuffix = '</span>';
+			const post = topic.children[refIndex];
+			const categoryHtml = `<span>${topic.alias}</span>`;
+			const postHtmlPrefix = '<span>';
+			const postHtmlSuffix = '</span>';
 
-            let postHtmlMiddle = '';
-            let from = 0;
-            let to = 0;
+			let postHtmlMiddle = '';
+			let from = 0;
+			let to = 0;
 
-            for (const index of indices) {
-                to = index[0];
+			for (const index of indices) {
+				to = index[0];
 
-                postHtmlMiddle += post.alias.slice(from, to);
-                postHtmlMiddle += `<mark>${post.alias.slice(index[0], index[1] + 1)}</mark>`;
+				postHtmlMiddle += post.alias.slice(from, to);
+				postHtmlMiddle += `<mark>${post.alias.slice(index[0], index[1] + 1)}</mark>`;
 
-                from = index[1] + 1;
-            }
+				from = index[1] + 1;
+			}
 
-            postHtmlMiddle += post.alias.slice(from);
+			postHtmlMiddle += post.alias.slice(from);
 
-            const postHtml = postHtmlPrefix + postHtmlMiddle + postHtmlSuffix;
-            const connectionHtml = '<span> - </span>';
-            const html = categoryHtml + connectionHtml + postHtml;
+			const postHtml = postHtmlPrefix + postHtmlMiddle + postHtmlSuffix;
+			const connectionHtml = '<span> - </span>';
+			const html = categoryHtml + connectionHtml + postHtml;
 
-            list.push({ html, topicName: topic.name, postName: post.name });
-        }
-    }
+			list.push({ html, topicName: topic.name, postName: post.name });
+		}
+	}
 
-    return list;
+	return list;
 }
 
 export default Search;
